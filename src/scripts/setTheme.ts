d6f77eedb8234ce6de7c5985b26360df6513cb1f
Adam-Elmi <Adamcade123@gmail.com>
2025-04-16 00:37:21 +0300

    Updated setTheme script

--- src/scripts/setTheme.ts
+++ src/scripts/setTheme.ts
@@ -1,6 +1,5 @@
 // Elements
 const html = document.querySelector("html");
-const body = document.body;
 const navBar = document.getElementById("nav-bar");
 const searchInputs = document.querySelectorAll("#search-container");
 const themeContainer = document.getElementById("theme-container");
@@ -8,13 +7,14 @@ const sideBar = document.getElementById("sidebar");
 const filterContainer = document.getElementById("filter-container");
 const listContainer = document.getElementById("list-container");
 const viewSection = document.getElementById("view-section");
+const footer = document.querySelector("footer");
 
 const colors = {
   c1: "#0f172b",
   c2: "#1d293d",
   c3: "#314158",
   c4: "#aad",
-  c5: "#615fff",
+  c5: "#0079FF",
   c6: "#1D2B53",
 }
 
@@ -23,8 +23,16 @@ type Styles = {
   textColor?: string;
   borderColor?: string;
 };
+type Directions = "top" | "left" | "right" | "bottom" | "tlrb";
+const directionsMap: any = {
+  top: "borderTopColor",
+  left: "borderLeftColor",
+  right: "borderRightColor",
+  bottom: "borderBottomColor",
+};
 
-function setStyle(element: any, styles: Styles = {}) {
+function setStyle(element: any, styles: Styles = {}, direction?: Directions, colors?:string[]) {
+  
   const { backgroundColor, textColor, borderColor } = styles;
   if(element) {
     const el = (element as HTMLElement);
@@ -35,9 +43,33 @@ function setStyle(element: any, styles: Styles = {}) {
     if(textColor) {
       el.style.color = textColor;
     }
+      if(direction && colors) {
+        switch(direction) {
+          case "top":
+            el.style[directionsMap.top] = colors[0];
+            break;
+          case "left":
+            el.style[directionsMap.left] = colors[0];
+            break;
+          case "right":
+            el.style[directionsMap.right] = colors[0];
+            break;
+          case "bottom":
+            el.style[directionsMap.bottom] = colors[0];
+            break;
+          case "tlrb":
+            el.style[directionsMap.top] = colors[0] ? colors[0] : "";
+            el.style[directionsMap.left] = colors[1] ? colors[1] : "";
+            el.style[directionsMap.right] = colors[2] ? colors[2] : "";
+            el.style[directionsMap.bottom] = colors[3] ? colors[3] : "";
+            break;
+        }
+      } else {
     if(borderColor) {
-      el.style.borderColor = borderColor;
+        el.style.borderColor = borderColor;
     }
+      }
+    
   } else {
     console.error(`${element} is not defined!`);
   }
@@ -51,18 +83,19 @@ function setTheme() {
         console.log("Light mode");
         break;
       case "dark":
+        setStyle(document.body, { backgroundColor: colors.c1 });
         setStyle(navBar, { backgroundColor: colors.c1, textColor: "white", borderColor: colors.c2 });
         searchInputs.forEach(input => {
           setStyle(input, { backgroundColor: colors.c3, textColor: "#fff", borderColor: colors.c2 });
           if (input.lastElementChild && input.lastElementChild.tagName === "BUTTON") {
-            setStyle(input.lastElementChild, { backgroundColor: colors.c5, borderColor: colors.c4 })
+            setStyle(input.lastElementChild, { backgroundColor: colors.c5, borderColor: "#777" })
           }
         });
         setStyle(themeContainer, { backgroundColor: "#0f172b" });
         setStyle(sideBar, { backgroundColor: colors.c1 });
         setStyle(filterContainer?.parentElement, { borderColor: colors.c2 });
         setStyle(filterContainer, { textColor: "#fff", borderColor: colors.c2, backgroundColor: colors.c3 });
-        setStyle(listContainer?.parentElement, { borderColor: colors.c3 });
+        setStyle(listContainer?.parentElement, { borderColor: colors.c2 });
         setStyle(listContainer, { textColor: "#fff", borderColor: colors.c3 });
         if(listContainer && listContainer.children.length > 0) {
           Array.from(listContainer.children).forEach(child => {
@@ -72,7 +105,13 @@ function setTheme() {
             }
           })
         };
-        setStyle(viewSection, { backgroundColor: colors.c1, textColor: "#fff" });
+        setStyle(viewSection, { backgroundColor: colors.c1, textColor: "#fff", borderColor: colors.c2 });
+        if(viewSection && viewSection.children.length > 0) {
+          Array.from(viewSection.children).forEach(child => {
+            
+          });
+        }
+        setStyle(footer, { backgroundColor: colors.c1}, "tlrb", [colors.c2, colors.c5]);
         console.log("Dark mode");
         break;
       default: {
